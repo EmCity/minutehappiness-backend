7d34e99d738b754d5cc7f4083b16a9fa3765abf6
Florian Schaule <f.schaule@gmail.com>
2017-06-23 22:54:34 +0200

    added youtube downloader added video to image converter

---
+++ youtube/download.py
@@ -0,0 +1,30 @@
+from pytube import YouTube
+import re 
+import os
+import ffmpy
+
+videos = []
+
+input_file = open('url.txt', 'r')
+for line in input_file:
+	videos.append(line.rstrip('\n'))
+
+print videos
+
+os.chdir('video')
+
+
+for counter, item in enumerate(videos):
+	yt = YouTube(item)
+	yt.set_filename('output')
+	video = yt.get('mp4', '720p')
+
+	url =  videos[counter]
+	regex = re.compile('\=')
+	start = regex.search(url).end()
+	end =  len(url)
+	video_id = url[start:end]
+	print video_id
+	if not os.path.exists(video_id):
+		os.makedirs(video_id)
+	video.download(str(video_id) + '/')
