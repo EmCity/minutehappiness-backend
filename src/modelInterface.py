89514818e9523826db32e7212b0fec39db3aeec4
frenski <frenski@gmail.com>
2017-06-24 12:58:49 +0200

    Fixed image load to process grayscale instead of RGB images

--- src/modelInterface.py
+++ src/modelInterface.py
@@ -5,8 +5,7 @@ import os
 import json
 
 def processImage(img):
-    img_array = cv2.imread(img)
-    # print(img_array)
+    img_array = cv2.imread(img,0)
     img_name = img.split('/')[3].split('.') 
     second = img_name[0]
     model_res = [0.5, 0.5]
@@ -20,5 +19,3 @@ def processImagesBatch(imgs):
         img_res = processImage(img)
         results.append(img_res)
     return results
-    
-    
