90d7d8b5661bb7aedb02ea617e02488dac42e619
Sebastian Wagner <sebastiantimwagner@gmail.com>
2017-06-24 12:10:55 +0200

    Finds highest sum of happiness scores and inserts the part of video with it.

--- src/convertFacesDataSlidingWindow.py
+++ src/convertFacesDataSlidingWindow.py
@@ -3,7 +3,7 @@ import writeToDBSlidingWindow as writeToDB
 import json
 import os
 import glob
-
+import operator
 
 
 '''
@@ -33,10 +33,10 @@ for root, dirs, files in os.walk(folder):
 		tracks[root] = frames
 		#for dir in dirs:
 			#for root, dirs, files in os.walk(folder+"/"+)
-print(tracks)
+#print(tracks)
 
 for f in tracks:
-	print("folder: ", f)
+	#print("folder: ", f)
 	#print(type(tracks))
 	images = tracks[f]
 	
@@ -45,7 +45,7 @@ for f in tracks:
 	#	print("frames: ", frames)
 	#print(images)
 	result = runFacesApi.returnTimestamps(images)
-	print("result", result)
+	#print("result", result)
 	#print result
 	#att = result[0][1]
 	#d = json.loads(att)
@@ -57,27 +57,44 @@ for f in tracks:
 	#writeToDB.writeDB(result, video_length) #write data to DB
 	videos = []
 	#Get the folder name
-	for r in result:
-		video = []
-		#att = result[0][1]
-		d = json.loads(r[1])
-		print("d", d)
-		filename = r[0].split('/')
-		frames = []
-		folder = filename[0]+"/"+filename[1]+"/"+filename[2]+"/"
-		print(folder)
-		#for root, dirs, files in os.walk(folder):
-		#	frames += glob.glob(os.path.join(root, '*.jpg'))
-		#print("frames: ", frames)
-		print(filename)
-		video_url = filename[2]
-		start_time = filename[3].split('.')[0]
-		#attJson = json.loads(att)
-		#print(att)
-		if d:
-			smile = d[0]['faceAttributes']['smile']
-			if(smile > 0.0):
-				video = [video_url, str(start_time), str(int(start_time)+video_length)]
-			videos.append(video)
-	#videos array is an array of (video_url, start_time, end_time) values
-	writeToDB.writeDB(videos)
+	if len(result) >= 10:	
+		scores = []
+		for r in result:
+			video = []
+			#att = result[0][1]
+			d = json.loads(r[1])
+			print("d", d)
+			filename = r[0].split('/')
+			frames = []
+			folder = filename[0]+"/"+filename[1]+"/"+filename[2]+"/"
+			print(folder)
+			#for root, dirs, files in os.walk(folder):
+			#	frames += glob.glob(os.path.join(root, '*.jpg'))
+			#print("frames: ", frames)
+			print(filename)
+			video_url = filename[2]
+			start_time = filename[3].split('.')[0]
+			#attJson = json.loads(att)
+			#print(att)
+			if d:
+				smile = d[0]['faceAttributes']['smile']
+				if(smile > 0.0):
+					#video = [video_url, str(start_time), str(int(start_time)+video_length)]
+					scores.append(smile)
+				#videos.append(video)
+		sums = []
+		idx = 0
+		for score in scores:
+			if idx+video_length < len(scores):
+				cnt = 0
+				for n in range(idx, idx+video_length):
+					cnt += scores[n]
+			sums.append(cnt)
+			idx = idx + 1
+		print("sums", sums, type(sums))
+		max_index, max_value = max(enumerate(sums), key=operator.itemgetter(1))
+		print('max_index', max_index, 'max_value', max_value)
+		video = [video_url, str(max_index), str(int(max_index)+video_length)]
+		videos.append(video)
+		#videos array is an array of (video_url, start_time, end_time) values
+		writeToDB.writeDB(videos)
