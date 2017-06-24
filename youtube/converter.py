e543bc7499f79534e4cba1208cd547101158f591
Florian Schaule <f.schaule@gmail.com>
2017-06-24 10:51:41 +0200

    added working faceextraction

--- youtube/converter.py
+++ youtube/converter.py
@@ -4,13 +4,14 @@ import os
 os.chdir('video')
 
 folders = os.listdir(os.getcwd())
-print folders
+print(folders)
 
 for video in folders:
-	 if not os.path.isfile(str(video) + '/output.mp4'): 
-	 	continue
-	 ff = FFmpeg(
-	 	inputs={str(video) + '/output.mp4': None},
-	 	outputs={str(video) + '/img%03d.jpg': '-vf fps=4'})
-	 print ff.cmd
-	 ff.run()
+    if not os.path.isfile(str(video) + '/output.mp4'):
+        continue
+    ff = FFmpeg(
+        inputs={str(video) + '/output.mp4': None},
+        outputs={str(video) + '/img%03d.jpg': '-qscale:v 4 -vf fps=1'})
+    print(ff.cmd)
+    ff.run()
+    os.remove(str(video) + '/output.mp4')
